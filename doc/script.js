b6d23ae31bdc864969c13960b2a6d89ddc5f9f55
Dokeyz <rikens1996@gmail.com>
2022-06-30 20:13:01 -0400

    added constants for each character type

--- doc/script.js
+++ doc/script.js
@@ -19,15 +19,40 @@ function writePassword() {
 
   //Adding if statement incase password is less than 8 characters or greater than 128 characters
   if (promptLength < 8 || promptLength > 128) {
+
+    //displays that the user must import a number greater than or equal to 8 and less than or equal to 128
     window.alert('Your password must be greater than 7 characters and less than 129 characters, please enter a valid number!');
+
+    //ends the inquiry if an invalid number is given:
     return;
   }
+  
+  
+  const lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  const numeric = ['0','1','2','3','4','5','6','7','8','9'];
+  const special = ['"',' ','!','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\',','^','_','`','{','|','}','~',']'];
+  
+  function askQuestions() {
 
-  var confirmLowercase = window.confirm('Would you like lowercase letters in your password? (Click "Ok" for yes or "Cancel" for no')
+    var options = []
 
-  if (confirmLowercase)
-    
+    //window asks if the user would like lowercase letters randomized into the password
+    var confirmLowercase = window.confirm('Would you like lowercase letters randomized into your password? (Click "Ok" for yes or "Cancel" for no')
 
+    //if the user clicks "OK" then it adds a constant array of lowercase strings to the variable "options" to be used in the password
+    if (confirmLowercase) {
+      options=options.concat(uppercase)
+    }
+
+
+    //window asks if the user would like UPPERCASE letters randomized into the password
+    var confirmUppercase = window.confirm('Would you like UPPERCASE letters randomized into your password? (Click "Ok" for yes or "Cancel" for no')
+
+    //if the user clicks "OK" then it adds a constant array of uppercase strings to the variable "options" to be used in the password
+    if (confirmUppercase) {
+     
+    }
       
 
 
